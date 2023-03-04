0316ba95717bae367790658c9bd34f1734045a51
DESKTOP-FJAKAS7\Siddhi <siddhigate16@gmail.com>
2023-03-04 15:44:32 +0530

    feat: google fonts api

--- script.js
+++ script.js
@@ -30,10 +30,12 @@ body.addEventListener("click", (e) => {
       optionsDiv.style.display = "inline-flex";
     }
   } else {
-    if(!e.target.attributes.unselect){
-        optionsDiv.style.display = 'none'
+    if (!e.target.attributes.unselect && !e.target.getAttribute("data-options")) {
+      optionsDiv.style.display = "none";
     }
-    
+  }
+  if(!e.target.getAttribute("data-options") && !e.target.getAttribute("data-is-text")) {
+    optionsDiv.style.display = "none";
   }
 });
 
@@ -61,12 +63,73 @@ optionsDiv.children[1].addEventListener("click", () => {
   }
 });
 
-optionsDiv.children[2].addEventListener('blur' , () => {
-    console.log(optionsDiv.children[2].value)
-    selectedElement.style.fontSize = `${(optionsDiv.children[2].value)}px`
-})
+optionsDiv.children[2].addEventListener("blur", () => {
+  console.log(optionsDiv.children[2].value);
+  selectedElement.style.fontSize = `${optionsDiv.children[2].value}px`;
+});
 
-optionsDiv.children[3].addEventListener('input' , () => {
-    console.log(optionsDiv.children[3].value)
-    selectedElement.style.color = optionsDiv.children[3].value
-})
+optionsDiv.children[3].addEventListener("input", () => {
+  console.log(optionsDiv.children[3].value);
+  selectedElement.style.color = optionsDiv.children[3].value;
+});
+
+// Google fonts
+
+const API_KEY = "AIzaSyDkxf_L1L2bOOVmztw_NcfY6eLmXNiZqAk";
+const apiUrl = `https://www.googleapis.com/webfonts/v1/webfonts?key=${API_KEY}&sort=popularity`;
+
+let allFonts = [];
+
+async function getGoogleFonts() {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  return data.items;
+}
+
+function applyFont(fontName, element) {
+  console.log(element)
+  element.style.fontFamily = fontName;
+}
+
+getGoogleFonts().then(async (fonts) => {
+  allFonts = fonts;
+  setFontOptions(fonts);
+});
+
+async function setFont(fontName) {
+  const selectedFont = allFonts.filter((font) => font?.family === fontName)[0];
+
+  if (selectedFont && selectedFont.files && selectedFont.files.regular) {
+    const fontContent = await fetch(selectedFont?.files.regular).then((r) =>
+      r.arrayBuffer()
+    );
+
+    const font = new FontFace(selectedFont.family, fontContent);
+    await font.load();
+    document.fonts.add(font);
+  }
+}
+
+const selectEl = document.querySelector("#select");
+
+function setFontOptions(fonts) {
+  fonts.forEach(async (font, index) => {
+    if (index > 200) {
+      return;
+    }
+
+    if (font.family && font.files && font.files.regular) {
+      const optionEl = document.createElement("option");
+      optionEl.setAttribute("value", font.family);
+      optionEl.textContent = font.family;
+      setFont(font?.family)
+      optionEl.style.fontFamily = font.family;
+      selectEl.append(optionEl);
+    }
+  });
+}
+
+selectEl.addEventListener("change", (e) => {
+    applyFont(e.target.value, selectedElement)
+});
+  
